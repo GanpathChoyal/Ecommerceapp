b6165ab71b8f560b7920e1b83cef16dda3a1c3ed
GanpathChoyal <ganpath9941899@gmail.com>
2025-12-10 22:28:23 +0530

    completed

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'store',
 ]
 
@@ -133,3 +135,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS=True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+}
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,20 +1,30 @@
-import { useEffect, useState } from "react";
+
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import ProductList from "../pages/ProductList";
-import ProductDetails from "../pages/ProductDetails";
+import ProductList from "./pages/ProductList";
+import ProductDetails from "./pages/ProductDetails";
+import Nabvar from './components/Navbar';
+import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import PrivateRouter from './components/PrivateRouter';
 
 function App() {
-  
-
-  return (
-    <Router>
-      <Routes>
-      <Route path="/" element={<ProductList/>}/>
-      <Route path="/product/:id" element={<ProductDetails/>}/>
-    </Routes>
-</Router>
-  );
-    
+    return (
+        <Router>
+            <Nabvar/>
+            <Routes>
+                <Route path="/" element={<ProductList/>}/>
+                <Route path="/product/:id" element={<ProductDetails/>}/>
+                <Route path="/cart" element={<CartPage/>}/>
+                <Route element={<PrivateRouter/>}>
+                    <Route path="/checkout" element={<CheckoutPage/>}/>
+                </Route>
+                <Route path="/login" element={<Login/>} />
+                <Route path="/signup" element={<Signup/>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
