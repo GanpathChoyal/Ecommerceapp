efff93cccb256687ab032e596533fef38b51af9b
GanpathChoyal <ganpath9941899@gmail.com>
2026-01-01 18:12:16 +0530

    some changes

--- Backend/store/apps.py
+++ Backend/store/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
-
+import os
 
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'store'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+
+        if os.environ.get("CREATE_SUPERUSER") == "True":
+            if not User.objects.filter(username="admin").exists():
+                User.objects.create_superuser(
+                    username="admin",
+                    email="admin@gmail.com",
+                    password="admin123"
+                )
